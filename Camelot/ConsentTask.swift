8dace75c7013d61f6e0527fa00c286be8765ac37
chris <c.winstanley90@gmail.com>
2015-05-27 14:19:51 +0100

    Added consent step

---
+++ Camelot/ConsentTask.swift
@@ -0,0 +1,29 @@
+//
+//  ConsentTask.swift
+//  Camelot
+//
+//  Created by Christopher Winstanley on 27/05/2015.
+//  Copyright (c) 2015 Winstanley. All rights reserved.
+//
+
+import ResearchKit
+
+public var ConsentTask: ORKOrderedTask {
+    
+    var steps = [ORKStep]()
+    
+    var consentDocument = ConsentDocument
+    let visualConsentStep = ORKVisualConsentStep(identifier: "VisualConsentStep", document: consentDocument)
+    steps += [visualConsentStep]
+    
+    let signature = consentDocument.signatures!.first as! ORKConsentSignature
+    
+    let reviewConsentStep = ORKConsentReviewStep(identifier: "ConsentReviewStep", signature: signature, inDocument: consentDocument)
+    
+    reviewConsentStep.text = "Review Consent!"
+    reviewConsentStep.reasonForConsent = "Consent to join study"
+    
+    steps += [reviewConsentStep]
+    
+    return ORKOrderedTask(identifier: "ConsentTask", steps: steps)
+}
--- Camelot/ViewController.swift
+++ Camelot/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ResearchKit
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func consentTapped(sender : AnyObject) {
+        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRunUUID: nil)
+        taskViewController.delegate = self
+        presentViewController(taskViewController, animated: true, completion: nil)
+    }
 }
+
+extension ViewController : ORKTaskViewControllerDelegate {
+    
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        //Handle results with taskViewController.result
+        taskViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
+
+
 
